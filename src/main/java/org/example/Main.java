b9c7930b2ebb5e894f266fbc1d6fda41df13a2ba
Alexandr <mailboxfav@gmail.com>
2023-07-11 20:54:50 +0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("10", "10"));
+        System.out.println(bins.mult("101", "1010"));//5*10=50//110110
+    }
+}
